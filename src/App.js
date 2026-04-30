9cb3e069d0f52da9969b2d6fbaa4102654d015b5
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-04-30 10:34:01 -0300

    feat: basic JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
 
 function App() {
+  const name = "Gabriel";
+
+  const newName = name.toUpperCase();
+
+  function sumNumber(a, b) {
+    return a + b;
+  }
+
+  const urlImage = 'https://placehold.co/400'
+
   return (
     <div className="App">
-      <h1>Olá react</h1>
-      <p>Meu primeiro APP fi</p>
+      <h1>Olá react</h1> 
+      <p>Olá, {newName}</p>
+      <p>Somador: {sumNumber(1, 5)}</p>
+      <img src={urlImage} alt="minha image" />
     </div>
   );
 }
